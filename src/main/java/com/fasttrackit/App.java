0444d8c9a0f4fd392c9dd2dbc6c017928f736e2a
Timi <timi.hasas@gmail.com>
2019-08-26 18:07:03 +0300

    Initial commit

---
+++ src/main/java/com/fasttrackit/App.java
@@ -0,0 +1,19 @@
+package com.fasttrackit;
+
+
+public class App
+{
+    public static void main(String[] args)
+
+    {
+        MathCalc total = new MathCalc();
+        System.out.println(total.Divide(20,5));
+        System.out.println(total.Extract(20,5));
+        System.out.println(total.Sum(20,5));
+        System.out.println(total.Multiply(20,5));
+
+    }
+
+}
+
+
---
+++ src/main/java/com/fasttrackit/MathCalc.java
@@ -0,0 +1,29 @@
+package com.fasttrackit;
+
+public class MathCalc {
+
+    public float Sum(int x, int y)
+    {
+        float toReturn = x + y;
+        return toReturn;
+    }
+
+    public float Divide(int x, int y)
+    {
+        float toReturn = x / y;
+        return toReturn;
+    }
+
+    public float Extract(int x, int y)
+    {
+        float toReturn = x - y;
+        return toReturn;
+    }
+
+    public float Multiply(int x, int y)
+    {
+        float toReturn = x * y;
+        return toReturn;
+    }
+
+}
